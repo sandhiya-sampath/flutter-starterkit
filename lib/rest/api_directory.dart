686af098cac3b87b05891b9e899125c63c7f2e84
Yogeshn067 <yogeshn067@gmail.com>
2023-12-14 17:08:51 +0530

    added comments

--- lib/rest/api_directory.dart
+++ lib/rest/api_directory.dart
@@ -3,8 +3,9 @@ import 'package:flutter_starter/util/environment_config.dart';
 //class defines urls for all apis used by the restful service layer
 
 class ApiDirectory {
-  static final ApiDirectory instance = ApiDirectory._();
+  //Private constructor
   ApiDirectory._();
+  static final ApiDirectory instance = ApiDirectory._();
   final String _baseUrl = EnvironmentConfig.apiUrl;
 
   //Simple Example
